5dbb70e3e40836bd24a26642d1c0427bc9e8e15f
Adryane Fernandes <edryfernandes@gmail.com>
2021-02-24 11:50:20 -0300

    Exercícios de interpretação feitos

---
+++ semana3/aula12/js-vanilla-template/index.js
@@ -0,0 +1,64 @@
+//Interpretação de código
+// EXERCÍCIO 1
+/*
+const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
+
+if(numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}
+    //O código pede um número ao usuário e verifica se é par. Se for par o usuário passa no teste, se for ímpar não passa. 
+*/
+
+
+// EXERCÍCIO 2
+/*
+let fruta = prompt("Escolha uma fruta")
+let preco
+switch (fruta) {
+  case "Laranja":
+    preco = 3.5
+    break;
+  case "Maçã":
+    preco = 2.25
+    break;
+  case "Uva":
+    preco = 0.30
+    break;
+  case "Pêra":
+    preco = 5.5
+    break; // BREAK PARA O ITEM c.
+  default:
+    preco = 5
+    break;
+}
+console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+*/
+    //a. Para dar o preço da fruta a partir do nome
+    //b. O preço da fruta maça é R$2.25
+    //c. O preço da fruta pêra é R$5 -  já que não tem break o código continuaria até o defalt e a var preco receberia 5
+
+
+// EXERCÍCIO 3
+/*
+const numero = Number(prompt("Digite o primeiro número."))
+
+if(numero > 0) {
+  console.log("Esse número passou no teste")
+	let mensagem = "Essa mensagem é secreta!!!"
+}
+
+console.log(mensagem)
+*/
+    //a. Pedindo ao usuário um número
+    //Essa mensagem é secreta
+
+    //b. Esse número passou no teste  
+    //Se for -10 não aparece nada
+
+
+//Escrita de código
+// EXERCÍCIO 4
+
