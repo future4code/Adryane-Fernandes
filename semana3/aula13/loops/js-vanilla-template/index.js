9f5adab311665f091f58ff022cab4038a79dc00c
Adryane Fernandes <edryfernandes@gmail.com>
2021-02-25 12:41:52 -0300

    Interpretação terminada

---
+++ semana3/aula13/loops/js-vanilla-template/index.js
@@ -0,0 +1,22 @@
+//Interpretação de texto
+//EXERCÍCIO 1
+/*
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
+*/
+    //O código está somando o contador à variável valor a cada loop
+    // será impresso: 15
+
+//EXERCÍCIO 2
+/*
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
+*/
+    //a. 19 21 23 25 27 30
