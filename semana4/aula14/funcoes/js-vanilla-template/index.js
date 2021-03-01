653b684e483dee7b318766d7dbd201e75fa59ce1
Adryane Fernandes <edryfernandes@gmail.com>
2021-03-01 16:39:11 -0300

    Comecei a fazer

---
+++ semana4/aula14/funcoes/js-vanilla-template/index.js
@@ -0,0 +1,17 @@
+//Interpretação de texto
+//EXERCÍCIO 1
+/*
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+*/
+    //a. 10 
+    //   50
+
+    //b.Não aconteria nada, já que a função n foi chamada
+
+//EXERCÍCIO 2
+ 
