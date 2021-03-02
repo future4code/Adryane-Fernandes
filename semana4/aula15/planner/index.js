d8a33eadf255aa8e656f897a4659ec359ab0b4fa
Adryane Fernandes <edryfernandes@gmail.com>
2021-03-02 15:54:30 -0300

    Desafios feitos

--- semana4/aula15/planner/index.js
+++ semana4/aula15/planner/index.js
@@ -2,6 +2,7 @@ function adicionarTarefa() {
     // Pegando os valores e areas do planner
     const tarefa = document.getElementById('tarefa')
     const opcaoDias = document.getElementById('dias-semana')
+    const horas = document.getElementById('horas')
 
     const domingo = document.getElementById('domingo')
     const segunda = document.getElementById('segunda')
@@ -11,39 +12,55 @@ function adicionarTarefa() {
     const sexta = document.getElementById('sexta')
     const sabado = document.getElementById('sabado')
 
-    // Verificando se esta vazio e colocando as tarefas
-    // if (tarefa !== "") {
-    switch (opcaoDias.value) {
-        case 'domingo':
-            domingo.innerHTML = `<p>${tarefa.value}</p>`
-            break;
+    const horaEscolhida = horas.value
+    // Verificando se esta vazio e colocando as tarefas no planner
+    if (tarefa.value !== '') {
+        switch (opcaoDias.value) {
+            case 'domingo':
+                domingo.innerHTML += `<p>${horaEscolhida}h</p> <p class='itemTarefa' onclick='tarefaConcluida()'>${tarefa.value}</p>`
+                break;
 
-        case 'segunda':
-            segunda.innerHTML = `<p> ${tarefa.value} </p>`
-            break;
-        
-        case 'terca':
-            terca.innerHTML = `<p> ${tarefa.value} </p>`
-            break;
-        
-        case 'quarta':
-            quarta.innerHTML = `<p> ${tarefa.value} </p>`
-            break;
+            case 'segunda':
+                segunda.innerHTML += `<p>${horaEscolhida}h</p> <p class='itemTarefa' onclick='tarefaConcluida()'>${tarefa.value}</p>`
+                break;
 
-        case 'quinta':
-            quinta.innerHTML = `<p> ${tarefa.value} </p>`
-            break;
-        
-        case 'sexta':
-            sexta.innerHTML = `<p> ${tarefa.value} </p>`
-            break;
-        
-        case 'sabado':
-            sabado.innerHTML = `<p> ${tarefa.value} </p>`
-            break;
+            case 'terca':
+                terca.innerHTML += `<p>${horaEscolhida}h</p> <p class='itemTarefa' onclick='tarefaConcluida()'>${tarefa.value}</p>`
+                break;
+
+            case 'quarta':
+                quarta.innerHTML += `<p>${horaEscolhida}h</p> <p class='itemTarefa' onclick='tarefaConcluida()'>${tarefa.value}</p>`
+                break;
+
+            case 'quinta':
+                quinta.innerHTML += `<p>${horaEscolhida}h</p> <p class='itemTarefa' onclick='tarefaConcluida()'>${tarefa.value}</p>`
+                break;
+
+            case 'sexta':
+                sexta.innerHTML += `<p>${horaEscolhida}h</p> <p class='itemTarefa' onclick='tarefaConcluida()'>${tarefa.value}</p>`
+                break;
+
+            case 'sabado':
+                sabado.innerHTML += `<p>${horaEscolhida}h</p> <p class='itemTarefa' onclick='tarefaConcluida()'>${tarefa.value}</p>`
+                break;
+        }
+
+        tarefa.value = ''
+    } else {
+        alert('Campo vazio. \nPor favor, adicione uma tarefa.')
     }
+}
 
-    tarefa.value = ''
+// Riscando o nome ao clicar
+function tarefaConcluida() {
+    document.querySelector('.div-item').classList.add('concluida')
+}
 
-    // }
+// Limpando a lista
+function limparLista() {
+    const lista = document.getElementsByClassName('div-itens')
+
+    for (const item of lista) {
+        item.innerHTML = ''
+    }
 }
