b8700edf50e60643ec96ecd4f2f5213d006e4534
Adryane Fernandes <edryfernandes@gmail.com>
2021-03-25 17:35:27 -0300

    site funcionando, as infos aparecem na tela

--- semana7/aula28/spacex2/src/App.js
+++ semana7/aula28/spacex2/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import axios from 'axios'
-import InfosCards from './components/InfosCards'
+import styled from 'styled-components'
+
+// const Container = styled.div `
+//   display:flex;
+//   align-items: center;
+//   justify-content: center;
+// `
 
 export default class App extends React.Component {
   state = {
     missions: [],
+    oneMission: {
+      description: '',
+      urlTwitter: '',
+      urlSite: '',
+      urlWikipedia: ''
+    }
   }
 
   componentDidMount = () => {
@@ -16,28 +28,47 @@ export default class App extends React.Component {
       const response = await axios.get('https://api.spacexdata.com/v3/missions')
 
       this.setState({ missions: response.data })
-      console.log(response.data)
 
     } catch (err) {
       console.log(err.response)
     }
   }
 
-  getOneMission = async () => {
-    
+  getOneMission = async (event) => {
+    try {
+      const id = event.target.value
+      const response = await axios.get(`https://api.spacexdata.com/v3/missions/${id}`)
+
+      this.setState({
+        description: response.data.description,
+        urlTwitter: response.data.twitter,
+        urlSite: response.data.website,
+        urlWikipedia: response.data.wikipedia
+      })
+
+    } catch (err) {
+      console.log(err.response)
+    }
   }
 
   render() {
     const missionsList = this.state.missions.map((mission) => {
-      return <option onClick={}>{mission.mission_name}</option>
+      return <option key={mission.mission_id} value={mission.mission_id}>
+        {mission.mission_name}
+      </option>
     })
 
     return (
-      <div className="App">
-        <select>
+      <div>
+        <select onChange={this.getOneMission}>
           <option >-</option>
           {missionsList}
         </select>
+        <p>{this.state.description}</p>
+
+        {(this.state.urlTwitter) && <a href={this.state.urlTwitter}>link TT</a>}
+        {(this.state.urlTwitter) && <a href={this.state.urlSite}>Link site</a>}
+        {(this.state.urlTwitter) && <a href={this.state.urlWikipedia}>Link site</a>}
       </div>
     );
   }
