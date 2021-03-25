7070920ca22fe8c1fbba1697a6a8773e92ef6464
Adryane Fernandes <edryfernandes@gmail.com>
2021-03-25 16:24:42 -0300

    Lista de missões aparece no select

--- semana7/aula28/spacex2/src/App.js
+++ semana7/aula28/spacex2/src/App.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import axios from 'axios'
+import InfosCards from './components/InfosCards'
 
 export default class App extends React.Component {
-  render(){
+  state = {
+    missions: []
+  }
+
+  componentDidMount = () => {
+    this.getMissions()
+  }
+
+  getMissions = async () => {
+    try {
+      const response = await axios.get('https://api.spacexdata.com/v3/missions')
+
+      this.setState({ missions: response.data })
+      console.log(response.data)
+
+    } catch (err) {
+      console.log(err.response)
+    }
+  }
+
+  render() {
+    const missionsList = this.state.missions.map((mission) => {
+      return <option>{mission.mission_name}</option>
+    })
+    
     return (
       <div className="App">
-        
+        <select>
+          <option >-</option>
+          {missionsList}
+        </select>
       </div>
     );
   }
