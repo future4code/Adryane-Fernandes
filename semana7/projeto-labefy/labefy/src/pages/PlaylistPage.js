1507be5b228e1d904c9a2bafcca27ea0585b227d
Adryane Fernandes <edryfernandes@gmail.com>
2021-03-26 16:29:15 -0300

    Mudei tudo para o PlaylistAndTracks e agr o get tracks funciona

--- semana7/projeto-labefy/labefy/src/pages/PlaylistPage.js
+++ semana7/projeto-labefy/labefy/src/pages/PlaylistPage.js
@@ -2,15 +2,11 @@ import React from 'react'
 // import styled from 'styled-components'
 // import axios from 'axios'
 // import { baseUrl, headers } from '../parameters'
-// import CreateTrackPage from './CreateTrackPage'
 
-
-// const OnePlaylist = styled.div`
-//     display: flex;
-//     justify-content: space-between;
-// `
 export default class PlaylistPage extends React.Component {
-    render() {
+  
+  
+  render() {
     return
   }
 }
