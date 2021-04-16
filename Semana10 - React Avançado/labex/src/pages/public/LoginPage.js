8673bf31a4eb518d5a5c5070733744dd9f8c6360
Adryane Fernandes <edryfernandes@gmail.com>
2021-04-15 22:14:52 -0300

    form do login mudado para useForm

--- Semana10 - React Avançado/labex/src/pages/public/LoginPage.js
+++ Semana10 - React Avançado/labex/src/pages/public/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import useInput from '../../hooks/useInput'
+// import useInput from '../../hooks/useInput'
+import useForm from '../../hooks/useForm'
 import Header from '../../components/Header'
 import login from '../../functions/login'
 import ImageBackground from '../../assents/backgroundLogin.jpg'
@@ -10,8 +11,12 @@ import { Input, Button } from '@chakra-ui/react'
 
 
 function LoginPage() {
-  const [email, handleLogin] = useInput('')
-  const [password, handlePassword] = useInput('')
+  const inicialState = {
+    email: '',
+    password: ''
+  }
+  const [form, onChange] = useForm(inicialState)
+
 
   const history = useHistory()
 
@@ -25,39 +30,49 @@ function LoginPage() {
 
   }, [history])
 
-
   const body = {
-    email: email,
-    password: password
+    email: form.email,
+    password: form.password
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(form)
+    login(history, body)
+  }
 
   return (
     <Container>
       <ContainerForm>
         <Header />
-        <Input
-          placeholder={'Email'}
-          value={email}
-          onChange={handleLogin}
-          type='email'
-        />
+        <form onSubmit={handleSubmit}>
+          <input
+            // required
+            name={'email'}
+            placeholder={'E-mail'}
+            value={form.email}
+            onChange={onChange}
+            type={'email'}
+          />
+          <input
+            // required
+            name={'password'}
+            placeholder={"Senha"}
+            value={form.password}
+            onChange={onChange}
+            type={'password'}
+          // pr="4.5rem"
+          // type={show ? "text" : "password"}
+          />
 
-        <Input
-          value={password}
-          onChange={handlePassword}
-          pr="4.5rem"
-          type={show ? "text" : "password"}
-          placeholder="Senha"
-        />
-        <Button h="1.75rem" size="sm" onClick={handleClick}>
-          {show ? "Mostrar senha" : "Esconder senha"}
-        </Button>
-        <Button onClick={() => login(history, body)}>
-          Entrar
-        </Button>
-        <Button onClick={history.goBack}>
-          voltar
-        </Button>
+          {/* <Button h="1.75rem" size="sm" onClick={handleClick}>
+            {show ? "Mostrar senha" : "Esconder senha"}
+          </Button> */}
+          <button>Entrar</button>
+          {/* <Button onClick={history.goBack}>
+            voltar
+          </Button> */}
+        </form>
       </ContainerForm>
 
 
