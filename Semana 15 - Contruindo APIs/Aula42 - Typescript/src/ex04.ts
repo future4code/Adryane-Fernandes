48064bff4943c5ca5705996b6916b0937368a08e
Adryane Fernandes <edryfernandes@gmail.com>
2021-05-18 15:12:27 -0300

    ex 4 pronto

---
+++ Semana 15 - Contruindo APIs/Aula42 - Typescript/src/ex04.ts
@@ -0,0 +1,7 @@
+// a) usaria o comando "tsc" no console
+
+// b) Não, já que o comando "tsc" faz a transpilação de todos os arquivos ts
+
+// c) tsc
+
+// d) algumas propriedades são descomentadas ou mudadas, como a versão do target, outDir e rootDir
