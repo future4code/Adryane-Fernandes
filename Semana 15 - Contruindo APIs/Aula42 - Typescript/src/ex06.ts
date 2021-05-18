546b5adb890f8e5a8142c092bb7e7aaf9b59c9bb
Adryane Fernandes <edryfernandes@gmail.com>
2021-05-18 17:46:12 -0300

    mudança no ex6

--- Semana 15 - Contruindo APIs/Aula42 - Typescript/src/ex06.ts
+++ Semana 15 - Contruindo APIs/Aula42 - Typescript/src/ex06.ts
@@ -1,26 +1,30 @@
+let historicalAge: string;
 function whatHistorcalAgeIsThatDate(year: number, complement?: string): string {
-  const date = new Date()
+  const date = new Date();
 
-  let historicalAge: string
-  if(year > 100000 && complement?.toLocaleLowerCase() === 'ac'){
-    historicalAge = 'Época fora dos registros'
-  } else if(year <= 100000 && year > 4000 && complement?.toLocaleLowerCase() === 'ac'){
-    historicalAge = 'Pré-história'
-  } else if(year <= 4000 && year > 476 && complement?.toLocaleLowerCase() === 'ac'){
-    historicalAge = 'Idade Antiga'
-  }else if(year <= 476 && year >= 0 && complement?.toLocaleLowerCase() === 'ac'){
-    historicalAge = 'Idade Média'
-  }else if(year > 0 && year <= 1453 && (complement?.toLocaleLowerCase() === 'dc' || !complement?.toLocaleLowerCase())){
-    historicalAge = 'Idade Média'
-  }else if(year > 1453 && year <= 1789 && (complement?.toLocaleLowerCase() === 'dc' || !complement?.toLocaleLowerCase())){
-    historicalAge = 'Idade Moderna'
-  }else if(year > 1789 &&  year <= date.getFullYear() && (complement?.toLocaleLowerCase() === 'dc' || !complement?.toLocaleLowerCase())){
-    historicalAge = 'Idade Contemporânea (até os dias atuais)'
-  }else{
-    historicalAge = 'O futuro teremos que esperar para saber'
+  if (complement?.toLocaleLowerCase() === "ac") {
+    if (year > 100000) {
+      historicalAge = "Época fora dos registros";
+    } else if (year <= 100000 && year > 4000) {
+      historicalAge = "Pré-história";
+    } else if (year <= 4000 && year > 476) {
+      historicalAge = "Idade Antiga";
+    } else if (year <= 476 && year >= 0) {
+      historicalAge = "Idade Média";
+    }
+  } else {
+    if (year > 0 && year <= 1453) {
+      historicalAge = "Idade Média";
+    } else if (year > 1453 && year <= 1789) {
+      historicalAge = "Idade Moderna";
+    } else if (year > 1789 && year <= date.getFullYear()) {
+      historicalAge = "Idade Contemporânea (até os dias atuais)";
+    } else {
+      historicalAge = "O futuro teremos que esperar para saber";
+    }
   }
 
-  return historicalAge
+  return historicalAge;
 }
 
-console.log(whatHistorcalAgeIsThatDate(2025, 'dc'))
+console.log(whatHistorcalAgeIsThatDate(1500, 'ac'));
