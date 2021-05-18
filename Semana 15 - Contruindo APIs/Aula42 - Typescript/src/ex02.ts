f3ad0d3f6611a3b9a2507fb64c74bcdbefaf9580
Adryane Fernandes <edryfernandes@gmail.com>
2021-05-18 14:58:21 -0300

    ex1 e 2 feitos

---
+++ Semana 15 - Contruindo APIs/Aula42 - Typescript/src/ex02.ts
@@ -0,0 +1,33 @@
+// a) 
+type estatistica = {
+  maior: number,
+  menor: number,
+  media: number
+}
+
+
+
+function obterEstatisticas(numeros: number[]): estatistica {
+  const numerosOrdenados: number[] = numeros.sort((a, b) => a - b )
+
+  let soma: number = 0
+
+  for (let num of numeros) {
+      soma += num
+  }
+
+  const estatisticas: estatistica = {
+      maior: numerosOrdenados[numeros.length - 1],
+      menor: numerosOrdenados[0],
+      media: soma / numeros.length
+  }
+
+  return estatisticas
+}
+
+const amostraDeIdades = {
+  numeros: [21, 18, 65, 44, 15, 18],
+  obterEstatisticas: obterEstatisticas
+}
+
+console.log(amostraDeIdades.obterEstatisticas(amostraDeIdades.numeros))
