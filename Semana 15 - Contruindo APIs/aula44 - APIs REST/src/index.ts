29284e28c7d78593bff7da4be7d0445eb3b2ea6e
Adryane Fernandes <edryfernandes@gmail.com>
2021-05-20 15:39:02 -0300

    ex2 feito

--- Semana 15 - Contruindo APIs/aula44 - APIs REST/src/index.ts
+++ Semana 15 - Contruindo APIs/aula44 - APIs REST/src/index.ts
@@ -6,10 +6,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/users", (req: Request, res: Response) => {
+app.get("/users/all", (req: Request, res: Response) => {
   res.status(200).send(users);
 });
 
+app.get("/users", (req: Request, res: Response) => {
+  try {
+    const type = String(req.query.type).toUpperCase()
+
+    if(type !== types.ADMIN && type !== types.NORMAL){
+      throw new Error("type must be admin or normal")
+    }
+
+    const result = users.filter((user) =>  user.type === type)
+
+    res.status(200).send(result)
+    
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
 
 app.listen(3003, () => {
   console.log("Server is running in http://localhost:3003");
