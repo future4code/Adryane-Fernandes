cb6e03deea3354d5a83a9c20d79435716040e64f
Adryane Fernandes <edryfernandes@gmail.com>
2021-05-20 15:14:59 -0300

    ex1 feito

--- Semana 15 - Contruindo APIs/aula44 - APIs REST/src/index.ts
+++ Semana 15 - Contruindo APIs/aula44 - APIs REST/src/index.ts
@@ -1,10 +1,16 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { users, types } from "./usersList";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/users", (req: Request, res: Response) => {
+  res.status(200).send(users);
+});
+
 
 app.listen(3003, () => {
-  console.log('Server is running in http://localhost:3003')
-})
+  console.log("Server is running in http://localhost:3003");
+});
