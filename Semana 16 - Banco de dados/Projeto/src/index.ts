37c4036e65f81defa8785796be29a08db89185b8
Adryane Fernandes <edryfernandes@gmail.com>
2021-05-29 17:24:52 -0300

    Pegar usuário por id

--- Semana 16 - Banco de dados/Projeto/src/index.ts
+++ Semana 16 - Banco de dados/Projeto/src/index.ts
@@ -2,13 +2,29 @@ import { Request, Response } from "express";
 import { app } from "./app";
 import { connection } from "./connection";
 
+app.get("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const result = await connection.raw(`
+      SELECT * FROM users
+      WHERE id = ${id}
+    `);
+
+    res.status(200).send(result[0]);
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
 app.put("/user", async (req: Request, res: Response) => {
   try {
-    const {name, nickname, email} = req.body
+    const { name, nickname, email } = req.body;
 
-    if(!name || !nickname || !email){
+    if (!name || !nickname || !email) {
       throw new Error("incomplete information");
     }
+
 
     await connection.raw(`
       INSERT INTO users (name, nickname, email)
@@ -17,9 +33,9 @@ app.put("/user", async (req: Request, res: Response) => {
         "${nickname}",
         "${email}"
       )
-    `)
+    `);
 
-    res.status(201).send("created user!")
+    res.status(201).send("created user");
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
