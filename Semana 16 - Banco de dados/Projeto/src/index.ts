237ed5abcfa2fa13b777bae4323eebfb28284691
Adryane Fernandes <edryfernandes@gmail.com>
2021-05-29 18:07:06 -0300

    atualizar informações criado

--- Semana 16 - Banco de dados/Projeto/src/index.ts
+++ Semana 16 - Banco de dados/Projeto/src/index.ts
@@ -17,6 +17,34 @@ app.get("/user/:id", async (req: Request, res: Response) => {
   }
 });
 
+app.post("/user/edit/:id", async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    const { name, nickname } = req.body;
+
+    if (!name || !nickname) {
+      throw new Error("incomplete information");
+    }
+
+    const users = await connection("users");
+    for (const user of users) {
+      if (user.nickname === nickname) {
+        throw new Error("nickname already exists");
+      }
+    }
+
+    await connection.raw(`
+      UPDATE users
+      SET name = "${name}", nickname = "${nickname}"
+      WHERE id = ${id}
+    `);
+
+    res.status(200).send("updated");
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
 app.put("/user", async (req: Request, res: Response) => {
   try {
     const { name, nickname, email } = req.body;
@@ -25,11 +53,11 @@ app.put("/user", async (req: Request, res: Response) => {
       throw new Error("incomplete information");
     }
 
-    const users = await connection("users")
+    const users = await connection("users");
     for (const user of users) {
-      if(user.nickname === nickname){
+      if (user.nickname === nickname) {
         throw new Error("nickname already exists");
-      } else if(user.email === email){
+      } else if (user.email === email) {
         throw new Error("email already exists");
       }
     }
