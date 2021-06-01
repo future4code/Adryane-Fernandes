78e81526356620fbcb53fe9c68e2e72702768872
Adryane Fernandes <edryfernandes@gmail.com>
2021-05-31 21:47:11 -0300

    pesquisa por nome funcionando

--- Semana 17 - Resolvendo Problemas/Aula49 - filtros, ordenação e paginação/src/index.ts
+++ Semana 17 - Resolvendo Problemas/Aula49 - filtros, ordenação e paginação/src/index.ts
@@ -2,8 +2,7 @@ import express, { Express, Request, Response } from "express";
 import knex from "knex";
 import cors from "cors";
 import dotenv from "dotenv";
-import { AddressInfo } from "net";
-import { recipe } from "./types/recipe";
+// import { AddressInfo } from "net";
 
 dotenv.config();
 
@@ -22,39 +21,47 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/recipes", async (req: Request, res: Response) => {
+app.get("/users/all", async (req: Request, res: Response) => {
   try {
-    const name = req.query.name as string;
+    const users = await connection.raw(`
+    SELECT id, name, email, type
+    FROM aula48_exercicio;
+ `);
 
-    if (!name) {
-       res.statusCode = 400
-      throw new Error("incomplete information");
-    }
-
-    const result = await connection.raw(`
-      SELECT * FROM aula48_exercicio
-      WHERE name LIKE "%${name}%";
-   `);
-
-    const recipes: recipe[] = result[0];
-
-    if (!recipes.length) {
+    if (!users[0].length) {
       res.statusCode = 404;
-      throw new Error("No recipes found");
+      throw new Error("No users found");
     }
 
-    res.status(200).send(recipes);
+    res.status(200).send(users);
   } catch (error) {
     console.log(error);
     res.status(400).send(error.message || error.sqlMessage);
   }
 });
 
-const server = app.listen(process.env.PORT || 3003, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Server is running in http://localhost:${address.port}`);
-  } else {
-    console.error(`Failure upon starting server.`);
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const name = req.query.name as string;
+
+    const [users] = await connection.raw(`
+    SELECT id, name, email, type
+    FROM aula48_exercicio
+    WHERE LOWER(name) LIKE "%${name.toLowerCase()}%";    
+ `);
+
+    // if (!users[0].length) {
+    //   res.statusCode = 404;
+    //   throw new Error("No users found");
+    // }
+
+    res.status(200).send(users);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error.message || error.sqlMessage);
   }
+});
+
+const server = app.listen(3003, () => {
+  console.log(`Server is running in http://localhost:3003`);
 });
