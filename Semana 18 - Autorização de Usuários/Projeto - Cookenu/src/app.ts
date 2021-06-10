4c040b210f6819dfa074c74507dd8c5a910205e4
Adryane Fernandes <edryfernandes@gmail.com>
2021-06-10 14:44:07 -0300

    initial commit

---
+++ Semana 18 - Autorização de Usuários/Projeto - Cookenu/src/app.ts
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+app.use(express.json())
+
+app.listen(3003, ()=> {
+  console.log("Server is running in http://localhost:3003")
+})
+
+export default app
