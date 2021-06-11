657bbe44175aa63d5e4c864920504be41de6a9ad
Adryane Fernandes <edryfernandes@gmail.com>
2021-06-11 14:17:07 -0300

    Login

--- Semana 18 - Autorização de Usuários/Projeto - Cookenu/src/validations/validateRequest.ts
+++ Semana 18 - Autorização de Usuários/Projeto - Cookenu/src/validations/validateRequest.ts
@@ -1,17 +1,24 @@
+
 export function validateRequest(name: string, data: string): void {
+  let message: string = "";
+  let itsError: boolean = false;
+
+  if (!data) {
+    message = `${name} not informed`;
+    itsError = true;
+  }
   if (name === "Email") {
     const regex =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!regex.test(data)) {
-      throw new Error("Invalid email format");
+      message = "Invalid email format";
+      itsError = true;
     }
   }
   if (name === "Password") {
     if (data.length < 6) {
-      throw new Error(`Password too short, it must be at least 6 characters`);
+      message = `Password too short, it must be at least 6 characters`;
+      itsError = true;
     }
-  }
-  if (!data) {
-    throw new Error(`${name} not informed`);
   }
 }
