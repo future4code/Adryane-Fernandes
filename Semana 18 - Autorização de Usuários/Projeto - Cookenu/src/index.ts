3ce71ee79e420427b65349016e7c89e4ee123727
Adryane Fernandes <edryfernandes@gmail.com>
2021-06-12 16:38:44 -0300

    deleteRecipe

--- Semana 18 - Autorização de Usuários/Projeto - Cookenu/src/index.ts
+++ Semana 18 - Autorização de Usuários/Projeto - Cookenu/src/index.ts
@@ -9,20 +9,19 @@ import followUser from "./endpoints/followUser";
 import unfollowUser from "./endpoints/unfollowUser";
 import getFeed from "./endpoints/getFeed";
 import editRecipe from "./endpoints/editRecipe";
+import deleteRecipe from "./endpoints/deleteRecipe";
 
 app.get("/user/profile", getProfile)
 app.get("/user/feed", getFeed)
 app.get("/user/:id", getOtherProfile)
-
 app.get("/recipe/:id", getRecipe)
-
 
 app.post("/signup", signup)
 app.post("/login", login)
 app.post("/user/follow", followUser)
 app.post("/user/unfollow", unfollowUser)
-
 app.post("/recipe", createRecipe)
 
-
 app.put("/recipe/:id/edit", editRecipe)
+
+app.delete("/recipe/:id/delete", deleteRecipe)
