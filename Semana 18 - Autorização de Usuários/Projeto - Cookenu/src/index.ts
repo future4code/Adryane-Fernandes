357e462f0a22bb2b6eaa6f5ff47bc1ec12a77b5a
Adryane Fernandes <edryfernandes@gmail.com>
2021-06-12 18:15:39 -0300

    resetPassword

--- Semana 18 - Autorização de Usuários/Projeto - Cookenu/src/index.ts
+++ Semana 18 - Autorização de Usuários/Projeto - Cookenu/src/index.ts
@@ -11,6 +11,7 @@ import getFeed from "./endpoints/getFeed";
 import editRecipe from "./endpoints/editRecipe";
 import deleteRecipe from "./endpoints/deleteRecipe";
 import deleteUser from "./endpoints/deleteUser";
+import resetPassword from "./endpoints/resetPassword";
 
 app.get("/user/profile", getProfile)
 app.get("/user/feed", getFeed)
@@ -23,6 +24,7 @@ app.post("/user/follow", followUser)
 app.post("/user/unfollow", unfollowUser)
 app.post("/recipe", createRecipe)
 
+app.put("/user/changePassword", resetPassword)
 app.put("/recipe/:id/edit", editRecipe)
 
 app.delete("/recipe/:id/delete", deleteRecipe)
