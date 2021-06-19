46ac855946adcb6314780da341c929f8c47e33da
Adryane Fernandes <edryfernandes@gmail.com>
2021-06-19 15:08:19 -0300

    Router feito

--- Semana 19 - Arquitetura de Software/Projeto - LaBook/src/index.ts
+++ Semana 19 - Arquitetura de Software/Projeto - LaBook/src/index.ts
@@ -1,15 +1,6 @@
-import { authenticationData, user, post } from "./model/types";
-import { hashCreate } from "./services/hashManager";
-import { generateId } from "./services/generateId";
-import { generateToken } from "./services/authenticator";
-import { signup } from "./controller/user/signup";
 import { app } from "./controller/app";
-import login from "./controller/user/login";
-import createPost from "./controller/post/createPost";
-import { searchPostById } from "./controller/post/searchPostById";
+import { userRouter } from "./router/userRouter";
+import { postRouter } from "./router/postRouter";
 
-app.post("/users/signup", signup);
-app.post("/users/login", login);
-
-app.post("/posts/create", createPost)
-app.get("/posts/:id", searchPostById)
+app.use('/users', userRouter)
+app.use('/posts', postRouter)
