e527ce73ab48ff9d5a8954dc8c5f709e116a58c6
Adryane Fernandes <edryfernandes@gmail.com>
2021-06-22 15:38:38 -0300

    Ex 1 e 2

---
+++ Semana 20 -  Testes/Aula60 - Testes unitários e mocks/src/validateCharacter.ts
@@ -0,0 +1,20 @@
+import { character } from "./model/interfaces";
+
+export function validateCharacter(input: character): boolean {
+  if (
+    !input.name ||
+    !input.life ||
+    !input.defense ||
+    !input.power
+  ) {
+    return false;
+  } else if (
+    input.life < 0 ||
+    input.power < 0 ||
+    input.defense < 0
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
